cef7be19f4b451e80fb50781ec159dc4f01a01f3
Justin Wheeler <justin.wheeler@wheelerswebservices.com>
2022-10-16 20:42:16 -0400

    updated to use selenim to fetch aws this week url

--- README.md
+++ README.md
@@ -18,16 +18,22 @@ https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=bot&permis
 sudo apt update
 sudo apt upgrade
 
+sudo apt install gnumeric
 sudo apt install libffi-dev
 sudo apt install python3-cffi
 sudo apt install python3-pip
+sudo apt install tigervnc-standalone-server 
+sudo apt install xvfb 
+sudo apt install xserver-xephyr 
+sudo apt install x11-utils 
 
 sudo pip3 install -U setuptools
-sudo pip3 install discord.py
+sudo pip3 install -r requirements.txt
 ```
 
 ## Run Bot
 
 ```
+export DISCORD_TOKEN='value'
 python3 wheebo.py
 ```
--- wheebo.py
+++ wheebo.py
@@ -1,20 +1,40 @@
 import discord
 import os
+import time
 
 from discord import app_commands
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+# discord setup
+client = discord.Client(intents=discord.Intents.default())
 tree = app_commands.CommandTree(client)
 
-@tree.command(name = "ping", description = "ping command")
-async def ping(interaction):
-    await interaction.response.send_message("pong")
+# selenium setup
+display = Display()
+display.start()
 
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+
+# define commands
+@tree.command(name = "aws-this-week", description = "What's new in AWS This Week?")
+async def awsThisWeek(interaction):
+    await interaction.response.defer(ephemeral=True, thinking=True)
+
+    driver.get('https://learn.acloud.guru/series/aws-this-week')
+    time.sleep(3)
+    
+    video_element = driver.find_element(By.CSS_SELECTOR, '.css-uwfm48 a')
+    video_link = video_element.get_attribute('href')
+
+    await interaction.followup.send(video_link)
+
+# sync commands
 @client.event
 async def on_ready():
     await tree.sync()
     print("Ready!")
 
+# start
 client.run(os.environ['DISCORD_TOKEN'])
