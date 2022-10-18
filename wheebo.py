a3f23c5a084e8a8f4fd9fb72bbe7078b220d6c2d
Justin Wheeler <wheelers_webistes@yahoo.com>
2022-10-18 08:14:47 -0400

    Updated to WheeBo to include Azure, GCP, K8s, and Linux

--- wheebo.py
+++ wheebo.py
@@ -18,17 +18,36 @@ display.start()
 driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
 
 # define commands
-@tree.command(name = "aws-this-week", description = "What's new in AWS This Week?")
-async def awsThisWeek(interaction):
+async def scrape(interaction, name):
     await interaction.response.defer(ephemeral=True, thinking=True)
 
-    driver.get('https://learn.acloud.guru/series/aws-this-week')
+    driver.get(f'https://learn.acloud.guru/series/${name}')
     time.sleep(3)
-    
+
     video_element = driver.find_element(By.CSS_SELECTOR, '.css-uwfm48 a')
     video_link = video_element.get_attribute('href')
 
-    await interaction.followup.send(video_link)
+    await interaction.followup.send(video_link, ephemeral=False)
+
+@tree.command(name = "aws-this-week", description = "What's new in AWS this Week?")
+async def aws_this_week(interaction):
+    await scrape(interaction, 'aws-this-week')
+
+@tree.command(name = "azure-this-week", description = "What's new in Azure this Week?")
+async def azure_this_week(interaction):
+    await scrape(interaction, 'azure-this-week')
+
+@tree.command(name = "gcp-this-month", description = "What's new in GCP this Month?")
+async def gcp_this_month(interaction):
+    await scrape(interaction, 'gcp-this-month')
+
+@tree.command(name = "kubernetes-this-month", description = "What's new in Kubernetes this Month?")
+async def kubernetes_this_month(interaction):
+    await scrape(interaction, 'kubernetes-this-month')
+
+@tree.command(name = "linux-this-month", description = "What's new in Linux this Month?")
+async def linux_this_month(interaction):
+    await scrape(interaction, 'linux-this-month')
 
 # sync commands
 @client.event
